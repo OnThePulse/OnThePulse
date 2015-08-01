7f83f888b856fe911577e87e4db7d990310ca5db
abkendal <kendal.adam@mail.com>
2015-08-01 18:20:26 -0400

    Connect function train to existing tweet alchemy functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 // Function:
 // 1. Choose city from drop down menu
 // 2. Query twitter for top 3 trending topics
-// 3. Query image api to find related image for each
-// 4. On click of a topic, query twitter api for most recent
+// 3. On click of a topic, query twitter api for most recent
 // tweets on that topic and compile together
- // 5. Query the alchemy api for sentiment and related concepts
+ // 4. Query the alchemy api for sentiment and related concepts
+ // 5. Search/display news articles relating to the trend
 
 
 
@@ -16,7 +16,7 @@ var flickrResponse = [];
 var trendImages = [];
 var testArray= ['Pizza', 'Ballon', 'Chess'];
 
-// Give the city selections functionality
+// 1. Give the city selections functionality
 app.citySelect = function(){
 	$('.city').on('click', function(evnt){
 		evnt.preventDefault();
@@ -27,7 +27,7 @@ app.citySelect = function(){
 	});
 };
 
-// Get the top 3 current trending topics for the city
+// 2. Get the top 3 current trending topics for the city
 app.getTrends = function(){
 	cityTrends=[];
 	$.ajax({
@@ -48,37 +48,6 @@ app.getTrends = function(){
 	})
 };
 
-// // Do a Flickr image search for each trend
-// app.getImages = function(trends) {
-// 	flickrResponse = [];
-// 	for (var i = 0; i < trends.length; i++){
-// 		console.log(trends[i]);
-// 		$.ajax({
-// 			url: 'https://api.flickr.com/services/rest',
-// 			type: 'GET',
-// 			dataType: 'jsonp',
-// 			jsonp: 'jsoncallback',
-// 			data: {
-// 				api_key: '7f330be3ad01149a9f99f92363a3b84b',
-// 				method: 'flickr.photos.search',
-// 				format: 'json',
-// 				tags: trends[i],
-// 				sort: 'relevance'
-// 			},
-// 			success: function(response){
-// 				console.log(response);
-// 				flickrResponse[i] = response;
-// 				// console.log(flickrResponse);
-// 				console.log(i);
-// 				if (i===2) {
-// 					console.log(flickrResponse);
-// 					app.displayImages(flickrResponse);
-// 				};
-// 			}
-// 		});
-// 	};
-// 	// console.log(flickrResponse);
-// };
 
 // Show the trends on the page
 app.showTrends = function(trends){
@@ -99,64 +68,43 @@ app.showTrends = function(trends){
 };
 
 
-
-
-// // Display the first image response on the page
-// app.displayImages = function(images){
-// 	console.log(images);
-// 	// https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-// 	$.each(images, function(index, item){
-// 		var farmId = item.photos.photo[0].farm;
-// 		// console.log(farmId);
-// 		var serverId = item.photos.photo[0].server;
-// 		var imgId = item.photos.photo[0].id;
-// 		var secret = item.photos.photo[0].secret;
-// 		var imgUrl = "https://farm"+farmId+".staticflickr.com/"+serverId+"/"+imgId+"_"+secret+".jpg";
-// 		console.log(umgUrl);
-// 		$('.trendImg'+ index).attr('src', imgUrl);
-// 	});
-// };
-
-
-
-
+// 3. On click of a topic, query twitter api for most recent
+// tweets on that topic and compile together
 app.trend0Select = function(){
 	$('.trend0').on('click', function(){
-		$('.trend1, .trend2').hide(1000);
+		// $('.trend1, .trend2').hide(1000);
 		$('.trend0').removeClass('col-sm-4').addClass('col-sm-12');
-
+		app.getTweets(cityTrends[0]);
 	});
 };
 
 app.trend1Select = function(){
 	$('.trend1').on('click', function(){
-		$('.trend0, .trend2').hide(1000);
+		// $('.trend0, .trend2').hide(1000);
 		$('.trend1').removeClass('col-sm-4').addClass('col-sm-12');
-		
+		app.getTweets(cityTrends[1]);
 	});
 };
 
 app.trend2Select = function(){
 	$('.trend2').on('click', function(){
-		$('.trend1, .trend0').hide(1000);
+		// $('.trend1, .trend0').hide(1000);
 		$('.trend2').removeClass('col-sm-4').addClass('col-sm-12');
-		
+		app.getTweets(cityTrends[2]);
 	});
 };
 
 
-
-
-app.getTweets = function(){
+app.getTweets = function(trend){
 	$.ajax({
 		url : 'http://localhost/twitterApp/tweets_json.php',
 		type: 'GET',
 		dataType: 'jsonp',
 		data: {
 			twitter_path: '1.1/search/tweets.json',		
-			q:'',
+			q: trend,
 			lang: 'en',
-			id: $city,
+			geocode: $cityGeo,
 			result_type: 'popular',
 			count: 75
 		},
@@ -179,7 +127,7 @@ app.compileTweets = function(tweets){
 
 	// Remove all twitter handles, urls and dashes. 
 	compiledTweet = compiledTweet.replace(/\@\S+/g,"").replace(/\http\S+/g,"").replace(/\-\S+/g,"");
-	// console.log(compiledTweet);
+	console.log(compiledTweet);
 	app.analyzeSentiment(compiledTweet);
 	app.analyzeConcepts(compiledTweet);
 };
@@ -197,7 +145,7 @@ app.analyzeSentiment = function(tweetText){
 		success: function(response){
 			var $sentimentResponse = $('<div>');
 			var $sentimentText = $('<p>');
-			$sentimentText.text("The current Nickelback sentiment is "+ response.docSentiment.type + " with a score of " + response.docSentiment.score);
+			$sentimentText.text("The current sentiment about this topic is "+ response.docSentiment.type + " with a score of " + response.docSentiment.score);
 			$sentimentResponse.append($sentimentText);
 			$('body').append($sentimentResponse);
 		}
@@ -239,3 +187,64 @@ app.init = function (){
 $(function(){
 	app.init();
 });
+
+
+
+
+
+// // Do a Flickr image search for each trend
+// app.getImages = function(trends) {
+// 	flickrResponse = [];
+// 	for (var i = 0; i < trends.length; i++){
+// 		console.log(trends[i]);
+// 		$.ajax({
+// 			url: 'https://api.flickr.com/services/rest',
+// 			type: 'GET',
+// 			dataType: 'jsonp',
+// 			jsonp: 'jsoncallback',
+// 			data: {
+// 				api_key: '7f330be3ad01149a9f99f92363a3b84b',
+// 				method: 'flickr.photos.search',
+// 				format: 'json',
+// 				tags: trends[i],
+// 				sort: 'relevance'
+// 			},
+// 			success: function(response){
+// 				console.log(response);
+// 				flickrResponse[i] = response;
+// 				// console.log(flickrResponse);
+// 				console.log(i);
+// 				if (i===2) {
+// 					console.log(flickrResponse);
+// 					app.displayImages(flickrResponse);
+// 				};
+// 			}
+// 		});
+// 	};
+// 	// console.log(flickrResponse);
+// };
+
+
+
+
+// // Display the first image response on the page
+// app.displayImages = function(images){
+// 	console.log(images);
+// 	// https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+// 	$.each(images, function(index, item){
+// 		var farmId = item.photos.photo[0].farm;
+// 		// console.log(farmId);
+// 		var serverId = item.photos.photo[0].server;
+// 		var imgId = item.photos.photo[0].id;
+// 		var secret = item.photos.photo[0].secret;
+// 		var imgUrl = "https://farm"+farmId+".staticflickr.com/"+serverId+"/"+imgId+"_"+secret+".jpg";
+// 		console.log(umgUrl);
+// 		$('.trendImg'+ index).attr('src', imgUrl);
+// 	});
+// };
+
+
+
+
+
+
